dc083ab072a0450ba7efff0220fa64c600527e93
Tristan Delforge <tristandelforge1@gmail.com>
2019-06-23 20:30:39 -0500

    added data

--- hr_data_manager.py
+++ hr_data_manager.py
@@ -6,6 +6,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from scipy import stats
 import time
+from zipfile import ZipFile
+import glob
+from fitparse import FitFile
 
 
 garmin_connect_sign_in_url = 'https://connect.garmin.com/signin/'
@@ -16,7 +19,6 @@ health_stats_css = 'body > div.connect-container > nav > div > ul.main-nav-group
 all_day_hr_css = 'body > div.connect-container > nav > div > ul.main-nav-group.main > li.main-nav-item.open-main-nav-menu > ul > li:nth-child(4) > a'
 gear_css = '#pageContainer > div > div.DailySummaryPage_headerContent__5RxH1 > div > div > div.flexboxgrid_col-xs-12__1qBu0.flexboxgrid_col-lg-8__2GN_e.flexboxgrid_collapse__OOU2Q > div > div.flexboxgrid_col-xs-3__3ojgO.flexboxgrid_col-md-4__1jqjd.flexboxgrid_collapse__OOU2Q > div:nth-child(2) > div.flexboxgrid_col-xs-12__1qBu0.flexboxgrid_collapse__OOU2Q.flexboxgrid_showAt-xs__tHwmF.flexboxgrid_showAt-sm__2c67f.flexboxgrid_showAt-md__5a-mB > div > div:nth-child(2) > div > button > i'
 export_css = '#pageContainer > div > div.DailySummaryPage_headerContent__5RxH1 > div > div > div.flexboxgrid_col-xs-12__1qBu0.flexboxgrid_col-lg-8__2GN_e.flexboxgrid_collapse__OOU2Q > div > div.flexboxgrid_col-xs-3__3ojgO.flexboxgrid_col-md-4__1jqjd.flexboxgrid_collapse__OOU2Q > div:nth-child(2) > div.flexboxgrid_col-xs-12__1qBu0.flexboxgrid_collapse__OOU2Q.flexboxgrid_showAt-xs__tHwmF.flexboxgrid_showAt-sm__2c67f.flexboxgrid_showAt-md__5a-mB > div > div:nth-child(2) > div > div > div:nth-child(2)'
-chrome_driver_path = r'C:\Users\trist\OneDrive\Desktop\hr_data_files\chromedriver.exe'
 
 
 def sleep_random_amount(min_time = 1.0, max_time = 4.0, mu = None, sigma = 1.0, verbose= False):
@@ -29,6 +31,11 @@ def sleep_random_amount(min_time = 1.0, max_time = 4.0, mu = None, sigma = 1.0, 
     if verbose:
         print('Sleeping for {0} seconds: {0}'.format(sleep_time))
     time.sleep(sleep_time)
+
+
+def extract_zip(input_zip):
+    input_zip=ZipFile(input_zip)
+    return {name: input_zip.read(name) for name in input_zip.namelist()}
 
 
 # class WebDriver:
@@ -46,26 +53,37 @@ class HRDataManager():
     def __init__(self, ):
         with open('personal', 'r') as f:
             lines = f.readlines()
-        self.user_name, self.password, self.data_file_loc, _ = lines[0].split('|')
+        self.user_name, self.password, self.data_file_loc, self.chrome_driver_path, _ = lines[0].split('|')
         self.load_all_data()
 
 
     def load_all_data(self):
         # get collected data
         # if days are missing, try and scrape them
+        self.process_zip_files()
+        self.read_files()
         pass
-
 
     def process_zip_files(self):
-        pass
-    
+        files = glob.glob('{0}/raw_zip_files/*'.format(self.data_file_loc))
+        for f in files:
+            with ZipFile(f, 'r') as zf:
+                zf.extractall('{0}/curated_files'.format(self.data_file_loc))
+
+    def read_files(self):
+        files = glob.glob('{0}/curated_files/*'.format(self.data_file_loc))
+        for f in files:
+            with FitFile(f, 'r') as ff:
+                print(ff.__dict__)
+
+
 
     def scrape_data(self):
         chrome_options = Options()
         chrome_options.add_argument("download.default_directory={}".format(self.data_file_loc))
         # chrome_options.add_argument("--headless")
 
-        with webdriver.Chrome(executable_path=chrome_driver_path, chrome_options=chrome_options) as driver:
+        with webdriver.Chrome(executable_path=self.chrome_driver_path, chrome_options=chrome_options) as driver:
             driver.get(garmin_connect_sign_in_url)
             driver.refresh()
             # print(1)
@@ -102,4 +120,4 @@ class HRDataManager():
 
 if __name__ == '__main__':
     data_manager = HRDataManager()
-    data_manager.scrape_data()
+    # data_manager.scrape_data()
